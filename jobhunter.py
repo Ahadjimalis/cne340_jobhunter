59381f950fbc016472e43d742e7793dc6836df8f
Ahadjimalis <83683366+Ahadjimalis@users.noreply.github.com>
2021-11-22 23:34:09 -0800

    Update jobhunter.py

--- jobhunter.py
+++ jobhunter.py
@@ -8,7 +8,7 @@ import html2text
 
 # Connect to database
 # You may need to edit the connect function based on your local settings.#I made a password for my database because it is important to do so. Also make sure MySQL server is running or it will not connect
-def connect_to_sql():
+def connect_to_sql(): #Created a database named "cne340"
     conn = mysql.connector.connect(user='root', password='',
                                    host='127.0.0.1', database='cne340')
     return conn
